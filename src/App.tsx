eeda7d8c25c7352cce6ebdd822b9331c94b20d7b
JeremyCPE <mohariry.jeremy@gmail.com>
2024-12-10 13:44:27 +0100

    remove footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,6 @@ function App() {
     <div className="min-h-screen bg-black flex py-8 px-4">
       <div className='max-w-6xl mx-auto'><TierList /></div>
     </div>
-    <footer className="bg-black text-white py-8">
-        <div className="container mx-auto px-4 text-left">
-          <p className="text-gray-400">©_RedSeeds @than_ontweeter</p>
-        </div>
-      </footer>
     </>
   )
 }
