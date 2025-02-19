ecafe826b154765bd3bde433aea73bd56907677f
vongo <vongo@andres-debian.home>
2025-02-19 21:16:27 +0100

    fix logos and install pkg via npm

---
+++ src/data/logo.data.ts
@@ -0,0 +1,40 @@
+import BDSA from "../assets/BDSA.png"
+import BKR from "../assets/BKR.png"
+import FNC from "../assets/FNC.png"
+import G2 from "../assets/G2.png"
+import GL from "../assets/GL.png"
+import GW from "../assets/GW.png"
+import GX from "../assets/GX.png"
+import IJC from "../assets/IJC.png"
+import JL from "../assets/JL.png"
+import KC from "../assets/KC.png"
+import M8 from "../assets/M8.png"
+import MKOI from "../assets/MKOI.png"
+import RGE from "../assets/RGE.png"
+import SK from "../assets/SK.png"
+import SLY from "../assets/SLY.png"
+import TH from "../assets/TH.png"
+import VIT from "../assets/VIT.png"
+
+export const teamLogos: Record<string, string> = {
+  "BDS": BDSA,
+  "BDSA": BDSA,
+  "BKR": BKR,
+  "FNC": FNC,
+  "G2": G2,
+  "GL": GL,
+  "GW": GW,
+  "GX": GX,
+  "IJC": IJC,
+  "JL": JL,
+  "KC": KC,
+  "KCB": KC,
+  "M8": M8,
+  "MKOI": MKOI,
+  "RGE": RGE,
+  "SK": SK,
+  "SLY": SLY,
+  "TH": TH,
+  "VIT": VIT,
+  "VITB": VIT
+};
