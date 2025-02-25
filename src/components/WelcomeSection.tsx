994e6c31078eaafee04beb7ba460daaeb717182d
vongo <vongo@andres-debian.home>
2025-02-25 12:34:50 +0100

    increase quick help width & when use click outside the help it closes it

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,19 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { HelpCircle } from 'lucide-react';
 
 export const WelcomeSection: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
 
+  // Ferme le menu si on clique ailleurs que sur les boutons de choix
+  const quickHelpRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (quickHelpRef.current && !quickHelpRef.current.contains(event.target as Node)) {
+        setIsVisible(false);
+      }
+    };
+
+    if (isVisible) {
+      document.addEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isVisible]);
+
+
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-4 right-4 z-50" ref={quickHelpRef}>
       {isVisible && (
-        <section className="absolute bottom-16 right-0 bg-gradient-to-br from-[#2a1f10] to-[#1a130a] border border-[#3d2d17] rounded-lg p-8 text-white max-w-3xl min-h-[300px] mb-4 shadow-xl">
+        <section className="absolute bottom-16 right-0 bg-gradient-to-br from-[#2a1f10] to-[#1a130a] border border-[#3d2d17] rounded-lg p-8 text-white w-[600px] min-h-[300px] mb-4 shadow-xl">
           <h2 className="text-2xl font-semibold mb-6 text-amber-200">Guide rapide</h2>
           <ul className="space-y-6 pl-6 list-disc text-xl text-gray-200">
             <li>
               Téléchargez le{' '}
-              <a 
-                href="https://github.com/JeremyCPE/TierLeague/tree/main/public/template" 
+              <a
+                href="https://github.com/JeremyCPE/TierLeague/tree/main/public/template"
                 className="font-semibold text-amber-400 hover:text-amber-300 transition-colors"
               >
                 template
